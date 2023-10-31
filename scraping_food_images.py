c593f676fbc9797287b8105578cd0a8c8b13f360
Koosms02 <Koosms02@gmail.com>
2023-10-31 17:57:09 +0200

    scraping_food

--- scraping_food_images.py
+++ scraping_food_images.py
@@ -6,17 +6,14 @@ from bs4 import BeautifulSoup
 
 
 
-folder_path = 'c:/Users/U20531232/Desktop/scraping _script/food/images/'
+folder_path = 'c:/Users/U20531232/Desktop/scraping _script/'
 
 file = os.listdir(folder_path)
 
-
-# items = soup.find_all('div' , class_ ="product__listing product__grid")
-
-#looping through the items
-
-# print(items)
-
+if os.path.exists(folder_path + str("images")):
+    print("paths exist")
+else:  
+    os.makedirs(folder_path + '/images' )
 
 def scrapeProducts(url ):
    
@@ -38,19 +35,33 @@ def scrapeProducts(url ):
 
             product_id = item['data-product-ga'].split('"id": "')[1].split('"')[0]
             product_name = ' '.join(parts[:-1])
-            product_price = item.find('div', class_='special-price__price').text.strip()
+            foldername = product_name 
+
+            # //create a folder with the product name 
+            if os.path.exists(folder_path + "/images"+"/"+foldername ):
+                print("The folder already exists")
+            else:
+                os.makedirs(folder_path + "/images"+"/"+foldername )
+
             product_image = 'https://www.shoprite.co.za/'+ item.find('div', class_='item-product__image __image').find('img')['data-original-src']
             
-            
-            print(f"Product ID: {product_id}")
-            print(f"Product Name: {product_name}")
-            print(f"Product Price: {product_price}")
-            print(f"Product Price: {product_image}")
+            # os.chdir(folder_path + "/images"+"/"+foldername )
+
+            imgRes = requests.get(product_image, stream=True)
+            filename = os.path.join(folder_path + "/images"+"/"+foldername , product_id+".png")
+
+# Save the image to the specified directory
+            with open(filename, 'wb') as image_file:
+                for chunk in imgRes.iter_content(chunk_size=8192):
+                  image_file.write(chunk)
+
+
+
             print("---")
 
 
 
-for  i in range(0 , 1):
+for  i in range(0 , 175):
     url = 'https://www.shoprite.co.za/c-2413/All-Departments/Food?q=%3Arelevance%3AbrowseAllStoresFacetOff%3AbrowseAllStoresFacetOff&page='+ str(i)
     scrapeProducts(url)
       
